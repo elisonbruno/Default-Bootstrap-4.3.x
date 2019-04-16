9124e60798edad09a241ca3b775ee0913815925a
Elison Bruno <elisonbrunosilva@gmail.com>
2019-04-16 11:54:00 -0300

    Upload files

---
+++ assets/inc/js.php
@@ -0,0 +1,3 @@
+<script src="assets/jquery/dist/jquery.min.js"></script>
+	<script src="assets/popper.js/dist/umd/popper.min.js"></script>
+	<script src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<!-- Metas -->
+	<?php include "assets/inc/metas.php" ?>
+
+	<!-- Favicon -->
+	<?php include "assets/inc/favicon.php" ?>
+
+	<?php include "assets/inc/head.php" ?>
+
+	<title>Title</title>
+</head>
+<body>
+	<header>Header</header>
+
+	<main>Main</main>
+
+	<footer>Footer</footer>
+
+	<?php include "assets/inc/js.php" ?>
+</body>
+</html>
